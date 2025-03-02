f916036f9d636f34af8af4efb38c677278501f3b
Iangallemit <gallemitkimian_bscs@plmun.edu.ph>
2025-03-02 14:43:27 +0800

    Submitting OBJPROG Week 04 - Session 01 - Exercise 02

--- SelectionStatementsDemo.java
+++ SelectionStatementsDemo.java
@@ -3,23 +3,23 @@ public class SelectionStatementsDemo {
     
     int studentScore = 82;
     if (studentScore >= 60) {
-   System.out.println("You passed the exam!");
-}
-if (studentScore >= 90) {
+        System.out.println("You passed the exam!");
+    }
+    if (studentScore >= 90) {
+        System.out.println("Excellent performance!");
+    } else {
+        System.out.println("Keep improving!");
+    }
+    if (studentScore >= 90) {
    System.out.println("Excellent performance!");
-} else {
-   System.out.println("Keep improving!");
-}
-if (studentScore >= 90) {
-   System.out.println("Excellent performance!");
-} else {
-   System.out.println("Keep improving!");
-}
+    } else {
+        System.out.println("Keep improving!");
+    }
 
-if (studentScore >= 60) {
-   if (studentScore >= 90) {
+    if (studentScore >= 60) {
+    if (studentScore >= 90) {
       System.out.println("Grade: A");
-   } else {
+    } else {
       if (studentScore >= 75) {
             System.out.println("Grade: B");
       } else {
